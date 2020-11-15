fc645c052e6cd7f89a4a0ac9a936b26b26b64a39
saurav ghimire <48352478+sauravvgh@users.noreply.github.com>
2020-11-15 15:51:01 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.WriteLine("Enter a color:");
+            color = Console.ReadLine();
+
+            Console.WriteLine("Enter a pluralNoun:");
+            pluralNoun = Console.ReadLine();
+
+            Console.WriteLine("Enter the name of a celebrity:");
+            celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("Roses are" +" " +color);
+            Console.WriteLine("Violets are" + " " +pluralNoun);
+            Console.WriteLine("I love" + " " + celebrity );
+            Console.ReadLine();
+                
+    
+
+        }
+    }
+}
